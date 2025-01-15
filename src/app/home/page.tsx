0d2014007170dba7c2a58c9cc47156a60bd9bf2f
Rikitost <kd1300674@st.kobedenshi.ac.jp>
2025-01-15 15:55:57 +0900

    test page

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="bg-white min-h-screen flex items-start justify-center px-4 md:px-20 py-3 md:py-5 pt-16">
+    </div>
+  );
+}
